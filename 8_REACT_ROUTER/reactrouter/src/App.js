1103a3e733073bd06019acf8752f1dbfbc4d31a2
Vinicius Benso <vinibenso@gmail.com>
2022-08-31 18:17:24 -0300

    finished react router module

--- 8_REACT_ROUTER/reactrouter/src/App.js
+++ 8_REACT_ROUTER/reactrouter/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 // 1 - config react router
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 //components
 import { Navbar } from './components/navbar/Navbar';
+import { SearchForm } from './components/search/SearchForm';
+
 
 //pages
 import { Home } from './pages/home/Home';
@@ -12,6 +14,7 @@ import { About } from './pages/about/About';
 import { Product } from './pages/product/Product';
 import { Info } from './pages/info/Info';
 import { Notfound } from './pages/notfound/NotFound';
+import { Search } from './pages/search/Search';
 
 
 
@@ -22,6 +25,8 @@ export function App() {
       {/*2 - Links com react router*/}
       <BrowserRouter>
         <Navbar />
+        {/*9 - Search */ }
+        <SearchForm />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -29,6 +34,10 @@ export function App() {
           <Route path="/products/:id" element={<Product />} />
           {/*6 - Nested Route */}
           <Route path="/products/:id/info" element={<Info />} />
+          {/* 9 - Search Page */}
+          <Route path="/search" element={<Search />} />
+          {/*10 - redirect */}
+          <Route path="/company" element={<Navigate  to='/about'/>} />
 
           {/*7 - no match route*/}
           <Route path="*" element={<Notfound />} />
--- 8_REACT_ROUTER/reactrouter/src/components/navbar/Navbar.js
+++ 8_REACT_ROUTER/reactrouter/src/components/navbar/Navbar.js
@@ -1,15 +1,19 @@
 // 2 - Links com react router
 import "./Navbar.css"
 
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export function Navbar() {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">Sobre</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/about">Sobre</Link> */}
+
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">Sobre</NavLink>
+
     </nav>
-    
-    
+
+
   )
 }
